27dedcd92674dcee2f2b087dd55b905347c16ddd
techiejd <62455039+techiejd@users.noreply.github.com>
2024-03-13 21:00:40 +0700

    And here we break ground

---
+++ programs/hot-potato/src/lib.rs
@@ -0,0 +1,118 @@
+use anchor_lang::prelude::*;
+use std::vec::Vec;
+
+use anchor_lang::error_code;
+
+#[error_code]
+pub enum HotPotatoError {
+    NotGameMaster,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
+pub enum GameState {
+    Pending,
+    /*
+    Staging {
+        start_time: u64,
+    },
+    Active {
+        round: u64,
+    },
+    Closed {
+        holding_potatoes: [Pubkey],
+        holding_stars: [PubKey],
+        end_time: u64,
+    },
+     */
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
+pub struct Turn {
+    pub player: Pubkey,
+    pub turn_number: u8,
+}
+
+#[account]
+pub struct Game {
+    pub game_master: Pubkey,        // 32 bytes
+    pub board: Option<Vec<Turn>>,   // 25 bytes
+    pub state: GameState,           // 1 byte
+    pub staging_period_length: u64, // 8 bytes
+    pub turn_period_length: u64,    // 8 bytes
+}
+
+// This is your program's public key and it will update
+// automatically when you build the project.
+declare_id!("9Y9Fcs7ixpxYYDdWZTKtXyEWJiUmDPmWPLsAT6bYvE4a");
+
+#[program]
+mod hot_potato {
+    use super::*;
+    pub fn initialize(
+        ctx: Context<InitializeGame>,
+        staging_period_length: u64,
+        turn_period_length: u64,
+    ) -> Result<()> {
+        *ctx.accounts.new_game = Game {
+            game_master: *ctx.accounts.game_master.key,
+            board: Some(Vec::new()),
+            state: GameState::Pending,
+            staging_period_length,
+            turn_period_length,
+        };
+        msg!("Game initialized and is now pending");
+        Ok(())
+    }
+
+    pub fn crank(ctx: Context<Crank>) -> Result<()> {
+        let game = &mut ctx.accounts.game;
+
+        require_keys_eq!(
+            game.game_master,
+            ctx.accounts.game_master.key(),
+            HotPotatoError::NotGameMaster
+        );
+
+        //game.crank(&tile)
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitializeGame<'info> {
+    #[account(init, seeds=[game_master.key().as_ref()], bump, payer = game_master, space = 8 + 74)]
+    pub new_game: Account<'info, Game>,
+    #[account(mut)]
+    pub game_master: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Crank<'info> {
+    #[account(mut)]
+    pub game: Account<'info, Game>,
+    #[account(signer)]
+    pub game_master: Signer<'info>,
+}
+
+/*
+// Make this a pda so we can know who is already playing
+pub struct ParticipationToken {
+    pub player: Pubkey,
+    // Is this really necessary or can we do it with msgs?
+    pub entry_amount: Lamports,
+    pub entered_round: u64,
+    pub exited_round: u64,
+}
+
+pub fn play(ctx: Context<Play>, tile: Tile) -> Result<()> {
+    let game = &mut ctx.accounts.game;
+
+    require_keys_eq!(
+        game.current_player(),
+        ctx.accounts.player.key(),
+        TicTacToeError::NotPlayersTurn
+    );
+
+    game.play(&tile)
+}*/
---
+++ tests/anchor.ts
@@ -0,0 +1,188 @@
+import assert from "assert";
+import * as web3 from "@solana/web3.js";
+import * as anchor from "@coral-xyz/anchor";
+import type { HotPotato } from "../target/types/hot_potato";
+import chai, { expect } from "chai";
+import BN from "chai-bn";
+import chaiAsPromised from "chai-as-promised";
+chai.use(BN(anchor.BN));
+chai.use(chaiAsPromised);
+
+describe("HotPotato", () => {
+  const oneDay: anchor.BN = new anchor.BN(86400); // seconds
+  const oneHour = new anchor.BN(3600); // seconds
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.HotPotato as anchor.Program<HotPotato>;
+  const confirmTx = async (txHash: string) => {
+    const latestBlockHash =
+      await program.provider.connection.getLatestBlockhash();
+
+    return await program.provider.connection.confirmTransaction({
+      blockhash: latestBlockHash.blockhash,
+      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+      signature: txHash,
+    });
+  };
+  const airdrop = async (addy: web3.PublicKey) => {
+    const airdropSignature = await program.provider.connection.requestAirdrop(
+      addy,
+      web3.LAMPORTS_PER_SOL
+    );
+    return confirmTx(airdropSignature);
+  };
+  const initGame = async (
+    stagingPeriodLength: anchor.BN,
+    turnPeriodLength: anchor.BN
+  ) => {
+    const gameMasterAccountKp = new web3.Keypair();
+
+    const [gameAccountPublicKey] = web3.PublicKey.findProgramAddressSync(
+      [gameMasterAccountKp.publicKey.toBuffer()],
+      program.programId
+    );
+    await airdrop(gameMasterAccountKp.publicKey);
+
+    const txHash = await program.methods
+      .initialize(stagingPeriodLength, turnPeriodLength)
+      .accounts({
+        newGame: gameAccountPublicKey,
+        gameMaster: gameMasterAccountKp.publicKey,
+        systemProgram: web3.SystemProgram.programId,
+      })
+      .signers([gameMasterAccountKp])
+      .rpc()
+      .catch((e) => {
+        console.log(anchor.translateError(e, new Map()));
+        throw Error(e);
+      })
+      .then((txHash) => txHash);
+    await confirmTx(txHash);
+    const gameAccount = await program.account.game.fetch(gameAccountPublicKey);
+    return { gameMasterAccountKp, gameAccountPublicKey, gameAccount, txHash };
+  };
+
+  it("fails initializing if gameMaster is not signer", async () => {
+    const gameMasterAccountKp = new web3.Keypair();
+    const someOtherAccountKp = new web3.Keypair();
+    const [gameAccountPublicKey] = web3.PublicKey.findProgramAddressSync(
+      [gameMasterAccountKp.publicKey.toBuffer()],
+      program.programId
+    );
+
+    await airdrop(gameMasterAccountKp.publicKey);
+    await expect(
+      program.methods
+        .initialize(oneDay, oneHour)
+        .accounts({
+          newGame: gameAccountPublicKey,
+          gameMaster: gameMasterAccountKp.publicKey,
+          systemProgram: web3.SystemProgram.programId,
+        })
+        .signers([someOtherAccountKp])
+        .rpc()
+    ).to.be.rejectedWith(
+      Error,
+      `unknown signer: ${someOtherAccountKp.publicKey.toString()}`
+    );
+  });
+
+  describe("Initializing", async () => {
+    let gameMasterAccountKp: web3.Keypair;
+    let gameAccount: Awaited<ReturnType<typeof program.account.game.fetch>>;
+    let txHash: string;
+    before(async () => {
+      const res = await initGame(oneDay, oneHour);
+      gameMasterAccountKp = res.gameMasterAccountKp;
+      gameAccount = res.gameAccount;
+      txHash = res.txHash;
+    });
+
+    it("has game master", () => {
+      assert(gameMasterAccountKp.publicKey.equals(gameAccount.gameMaster));
+    });
+    it("has status is pending", () => {
+      expect(gameAccount.state).to.eql({ pending: {} });
+    });
+    it("has an empty board", async () => {
+      expect(gameAccount.board).to.eql([]);
+    });
+    it("has a staging period", async () => {
+      expect(gameAccount.stagingPeriodLength).to.be.a.bignumber.that.eq(oneDay);
+    });
+    it("has a turn period", async () => {
+      expect(gameAccount.turnPeriodLength).to.be.a.bignumber.that.eq(oneHour);
+    });
+    it("logs state starting and pending", async () => {
+      const txDetails = await program.provider.connection.getTransaction(
+        txHash,
+        {
+          maxSupportedTransactionVersion: 0,
+          commitment: "confirmed",
+        }
+      );
+      const logs = txDetails?.meta?.logMessages;
+      expect(logs.join("\n")).to.include("Game initialized and is now pending");
+    });
+  });
+
+  describe("Playing", () => {
+    it("fails cranking if gameMaster is not signer", async () => {
+      const { gameMasterAccountKp, gameAccountPublicKey } = await initGame(
+        oneDay,
+        oneHour
+      );
+      const someOtherAccountKp = new web3.Keypair();
+
+      await expect(
+        program.methods
+          .crank()
+          .accounts({
+            game: gameAccountPublicKey,
+            gameMaster: gameMasterAccountKp.publicKey,
+          })
+          .signers([someOtherAccountKp])
+          .rpc()
+      ).to.be.rejectedWith(
+        Error,
+        `unknown signer: ${someOtherAccountKp.publicKey.toString()}`
+      );
+    });
+    it("only allows initial game master to crank", async () => {
+      const { gameAccountPublicKey } = await initGame(oneDay, oneHour);
+      const someOtherAccountKp = new web3.Keypair();
+
+      await expect(
+        program.methods
+          .crank()
+          .accounts({
+            game: gameAccountPublicKey,
+            gameMaster: someOtherAccountKp.publicKey,
+          })
+          .signers([someOtherAccountKp])
+          .rpc()
+      ).to.be.rejectedWith(anchor.AnchorError, "NotGameMaster");
+    });
+    it("cannot crank while pending", async () => {});
+    it("game master cannot play", async () => {});
+    it("changes status to staging when first player joins", async () => {});
+    it("logs status change to staging", async () => {});
+    it("logs player joining and amount", async () => {});
+    it("fills board with player's turns", async () => {});
+    it("allows first player to join again");
+    it("allows second player to join");
+    it("does not allow first crank until after staging period", async () => {});
+    it("changes status to active on first crank", async () => {});
+    it("sends SOL to first and second player after first crank", async () => {});
+    it("allows third player to join", async () => {});
+    it("sends SOL to first, second and third player in second crank", async () => {});
+    it("allows for game master to take out the game master money", async () => {});
+  });
+
+  describe("Finishing", () => {
+    it("changes state to closing", async () => {});
+    it("logs state change to closing", async () => {});
+    it("it does not allow new players to join", async () => {});
+    it("allows for game master to take out the game master money", async () => {});
+  });
+});
