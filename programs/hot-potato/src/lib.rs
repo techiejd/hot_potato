51df51d7dd0fe812fd604150aebcb9ed939843c7
techiejd <62455039+techiejd@users.noreply.github.com>
2024-03-16 23:44:00 +0700

    Adds first play

--- programs/hot-potato/src/lib.rs
+++ programs/hot-potato/src/lib.rs
@@ -1,5 +1,8 @@
 use anchor_lang::prelude::*;
-use std::vec::Vec;
+use anchor_lang::system_program::transfer;
+use anchor_lang::system_program::Transfer;
+use solana_program::sysvar::clock::Clock;
+use std::{ops::Add, vec::Vec};
 
 use anchor_lang::error_code;
 
@@ -8,16 +11,20 @@ pub enum HotPotatoError {
     NotGameMaster,
     CannotCrankWhilePending,
     GameMasterCannotPlay,
-    InsufficientFunds,
+    BelowTicketEntryMinimum,
+    GameBoardFull,
 }
 
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
+mod constants {
+    pub const NUM_TURNS: u64 = 150;
+    pub const NONUNIQUE_POTATO_HOLDERS_MAX: u64 = 10_000;
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq, Copy)]
 pub enum GameState {
     Pending,
+    Staging { ending: i64 },
     /*
-    Staging {
-        start_time: u64,
-    },
     Active {
         round: u64,
     },
@@ -29,20 +36,89 @@ pub enum GameState {
      */
 }
 
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
-pub struct Turn {
-    pub player: Pubkey,
-    pub turn_number: u8,
+#[derive(
+    AnchorSerialize, AnchorDeserialize, Clone, Debug, Copy, bytemuck::Pod, bytemuck::Zeroable,
+)]
+#[repr(C)]
+pub struct PotatoHoldingInformation {
+    pub player: Pubkey,   // 32 bytes
+    pub turn_number: u64, // 8 bytes
+    pub turn_amount: u64, // 8 bytes
+}
+
+#[account(zero_copy)]
+#[repr(C)]
+pub struct Board {
+    head: u64,                                          // 8 bytes
+    tail: u64,                                          // 8 bytes
+    pub owning_game_key: Pubkey,                        // 32 bytes
+    potato_holders: [PotatoHoldingInformation; 10_000], // 480_000 bytes. There's a parsing bug which is why we don't use constants::NONUNIQUE_POTATO_HOLDERS_MAX
+}
+
+impl Board {
+    fn next_end(&self) -> u64 {
+        (self.tail + 1) % constants::NONUNIQUE_POTATO_HOLDERS_MAX
+    }
+    pub fn push(&mut self, potato_holding_information: PotatoHoldingInformation) -> Result<()> {
+        // This function adds a potato holding information to the end of the board in a round-robin fashion
+        let next_end = self.next_end();
+        require_neq!(next_end, self.head, HotPotatoError::GameBoardFull);
+        self.potato_holders[self.tail as usize] = potato_holding_information;
+        self.tail = next_end;
+        Ok(())
+    }
 }
 
 #[account]
 pub struct Game {
-    pub game_master: Pubkey,        // 32 bytes
-    pub board: Option<Vec<Turn>>,   // 25 bytes
-    pub state: GameState,           // 1 byte
-    pub staging_period_length: u64, // 8 bytes
-    pub turn_period_length: u64,    // 8 bytes
+    pub staging_period_length: i64, // 8 bytes
+    pub turn_period_length: i64,    // 8 bytes
     pub minimum_ticket_entry: u64,  // 8 bytes
+    pub state: GameState,           // 16 byte
+    pub game_master: Pubkey,        // 32 bytes
+    pub board: Pubkey,              // 32 bytes
+}
+
+impl Game {
+    pub const SIZE: usize = 8 + 8 + 8 + 16 + 32 + 32;
+
+    pub fn request_hot_potato<F0, F1>(
+        &mut self,
+        player: &Pubkey,
+        ticket_entry: u64,
+        charge_ticket_entry: F0,
+        push_potato_holding_information: F1,
+    ) -> Result<()>
+    where
+        F0: FnOnce() -> Result<()>,
+        F1: FnOnce(PotatoHoldingInformation) -> Result<()>,
+    {
+        let give_player_hot_potato = || -> Result<()> {
+            charge_ticket_entry()?;
+            push_potato_holding_information(PotatoHoldingInformation {
+                player: *player,
+                turn_number: 0,
+                turn_amount: ticket_entry / constants::NUM_TURNS,
+            })?;
+            msg!("Player {} joined with {}", player, ticket_entry);
+            Ok(())
+        };
+        match self.state {
+            GameState::Pending => {
+                give_player_hot_potato()?;
+                self.state = GameState::Staging {
+                    ending: Clock::get()
+                        .expect("No system clock time")
+                        .unix_timestamp
+                        .add(self.staging_period_length),
+                };
+                msg!("Game is now in staging mode");
+            }
+            _ => {}
+        }
+
+        Ok(())
+    }
 }
 
 // This is your program's public key and it will update
@@ -54,18 +130,20 @@ mod hot_potato {
     use super::*;
     pub fn initialize(
         ctx: Context<InitializeGame>,
-        staging_period_length: u64,
-        turn_period_length: u64,
+        staging_period_length: i64,
+        turn_period_length: i64,
         minimum_ticket_entry: u64,
     ) -> Result<()> {
         *ctx.accounts.new_game = Game {
             game_master: *ctx.accounts.game_master.key,
-            board: Some(Vec::new()),
+            board: *ctx.accounts.new_board.to_account_info().key,
             state: GameState::Pending,
             staging_period_length,
             turn_period_length,
             minimum_ticket_entry,
         };
+        ctx.accounts.new_board.load_init()?.owning_game_key =
+            *ctx.accounts.new_game.to_account_info().key;
         msg!("Game initialized and is now pending");
         Ok(())
     }
@@ -90,24 +168,55 @@ mod hot_potato {
 
     pub fn request_hot_potato(ctx: Context<RequestHotPotato>, ticket_entry: u64) -> Result<()> {
         let game = &mut ctx.accounts.game;
+        let game_account_info = game.to_account_info().clone();
+        let player = &ctx.accounts.player;
+        let board = &mut ctx.accounts.board.load_mut()?;
         require_keys_neq!(
             game.game_master,
-            ctx.accounts.player.key(),
+            player.key(),
             HotPotatoError::GameMasterCannotPlay
         );
-        require_gt!(
+        require_keys_eq!(
+            board.owning_game_key,
+            game_account_info.key(),
+            HotPotatoError::NotGameMaster
+        );
+        require_gte!(
             ticket_entry,
             game.minimum_ticket_entry,
-            HotPotatoError::InsufficientFunds
+            HotPotatoError::BelowTicketEntryMinimum
         );
-        Ok(())
+        let chump_change = ticket_entry % constants::NUM_TURNS;
+        let actual_ticket_entry = ticket_entry - chump_change;
+        let charge_ticket_entry = || {
+            let cpi_context = CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                Transfer {
+                    from: player.to_account_info().clone(),
+                    to: game_account_info,
+                },
+            );
+            transfer(cpi_context, actual_ticket_entry)
+        };
+        let push_potato_holding_information =
+            |potato_holding_information: PotatoHoldingInformation| {
+                board.push(potato_holding_information)
+            };
+        game.request_hot_potato(
+            &player.key(),
+            actual_ticket_entry,
+            charge_ticket_entry,
+            push_potato_holding_information,
+        )
     }
 }
 
 #[derive(Accounts)]
 pub struct InitializeGame<'info> {
-    #[account(init, seeds=[game_master.key().as_ref()], bump, payer = game_master, space = 8 + 82)]
+    #[account(init, seeds=[game_master.key().as_ref()], bump, payer = game_master, space = 8 + Game::SIZE)]
     pub new_game: Account<'info, Game>,
+    #[account(zero)]
+    pub new_board: AccountLoader<'info, Board>,
     #[account(mut)]
     pub game_master: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -125,28 +234,9 @@ pub struct Crank<'info> {
 pub struct RequestHotPotato<'info> {
     #[account(mut)]
     pub game: Account<'info, Game>,
-    #[account(signer)]
+    #[account(mut)]
+    pub board: AccountLoader<'info, Board>,
+    #[account(mut, signer)]
     pub player: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
-
-/*
-// Make this a pda so we can know who is already playing
-pub struct ParticipationToken {
-    pub player: Pubkey,
-    // Is this really necessary or can we do it with msgs?
-    pub entry_amount: Lamports,
-    pub entered_round: u64,
-    pub exited_round: u64,
-}
-
-pub fn play(ctx: Context<Play>, tile: Tile) -> Result<()> {
-    let game = &mut ctx.accounts.game;
-
-    require_keys_eq!(
-        game.current_player(),
-        ctx.accounts.player.key(),
-        TicTacToeError::NotPlayersTurn
-    );
-
-    game.play(&tile)
-}*/
--- tests/anchor.ts
+++ tests/anchor.ts
@@ -11,7 +11,9 @@ chai.use(chaiAsPromised);
 describe("HotPotato", () => {
   const oneDay: anchor.BN = new anchor.BN(86400); // seconds
   const oneHour = new anchor.BN(3600); // seconds
+  const bigNumZero = new anchor.BN(0);
   const minimumTicketEntry = new anchor.BN(web3.LAMPORTS_PER_SOL / 2);
+  const NumTurns = 150;
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.HotPotato as anchor.Program<HotPotato>;
@@ -19,7 +21,7 @@ describe("HotPotato", () => {
     const latestBlockHash =
       await program.provider.connection.getLatestBlockhash();
 
-    return await program.provider.connection.confirmTransaction({
+    return program.provider.connection.confirmTransaction({
       blockhash: latestBlockHash.blockhash,
       lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
       signature: txHash,
@@ -28,26 +30,69 @@ describe("HotPotato", () => {
   const airdrop = async (addy: web3.PublicKey) => {
     const airdropSignature = await program.provider.connection.requestAirdrop(
       addy,
-      web3.LAMPORTS_PER_SOL
+      5 * web3.LAMPORTS_PER_SOL
     );
     return confirmTx(airdropSignature);
+  };
+  const initializeBoardAccount = async (gameMasterAccountKp: web3.Keypair) => {
+    const boardAccountKp = new web3.Keypair();
+    const boardAccountSize = program.account.board.size;
+    const lamportsForRentExemption =
+      await program.provider.connection.getMinimumBalanceForRentExemption(
+        boardAccountSize
+      );
+    const createAccountInstruction = web3.SystemProgram.createAccount({
+      fromPubkey: gameMasterAccountKp.publicKey,
+      newAccountPubkey: boardAccountKp.publicKey,
+      lamports: lamportsForRentExemption,
+      space: boardAccountSize,
+      programId: program.programId,
+    });
+    const transaction = new web3.Transaction().add(createAccountInstruction);
+    await web3.sendAndConfirmTransaction(
+      program.provider.connection,
+      transaction,
+      [gameMasterAccountKp, boardAccountKp]
+    );
+    return boardAccountKp;
+  };
+  const expectEmptyBoardSlot = (holder: {
+    player: web3.PublicKey;
+    turnNumber: anchor.BN;
+    turnAmount: anchor.BN;
+  }) => {
+    const emptyBoardSlot = {
+      player: web3.SystemProgram.programId,
+      turnNumber: bigNumZero,
+      turnAmount: bigNumZero,
+    };
+    expect(holder).to.have.property("player").and.to.eql(emptyBoardSlot.player);
+    expect(holder)
+      .to.have.property("turnNumber")
+      .and.to.be.a.bignumber.that.is.eq(emptyBoardSlot.turnNumber);
+    expect(holder)
+      .to.have.property("turnAmount")
+      .and.to.be.a.bignumber.that.is.eq(emptyBoardSlot.turnAmount);
   };
   const initGame = async (
     stagingPeriodLength: anchor.BN,
     turnPeriodLength: anchor.BN
   ) => {
     const gameMasterAccountKp = new web3.Keypair();
+    await airdrop(gameMasterAccountKp.publicKey);
+
+    const boardAccountKp = await initializeBoardAccount(gameMasterAccountKp);
 
     const [gameAccountPublicKey] = web3.PublicKey.findProgramAddressSync(
       [gameMasterAccountKp.publicKey.toBuffer()],
       program.programId
     );
-    await airdrop(gameMasterAccountKp.publicKey);
 
     const txHash = await program.methods
       .initialize(stagingPeriodLength, turnPeriodLength, minimumTicketEntry)
       .accounts({
         newGame: gameAccountPublicKey,
+        newBoard: boardAccountKp.publicKey,
         gameMaster: gameMasterAccountKp.publicKey,
         systemProgram: web3.SystemProgram.programId,
       })
@@ -60,25 +105,77 @@ describe("HotPotato", () => {
       .then((txHash) => txHash);
     await confirmTx(txHash);
     const gameAccount = await program.account.game.fetch(gameAccountPublicKey);
-    return { gameMasterAccountKp, gameAccountPublicKey, gameAccount, txHash };
+    const boardAccount = await program.account.board.fetch(
+      boardAccountKp.publicKey
+    );
+    return {
+      gameMasterAccountKp,
+      gameAccountPublicKey,
+      boardAccountPublicKey: boardAccountKp.publicKey,
+      gameAccount,
+      boardAccount,
+      boardAccountKp,
+      txHash,
+    };
   };
 
   const initLongGame = async () => initGame(oneDay, oneHour);
 
+  const doFirstPlayerRequestHotPotato = async (
+    gameAccountPublicKey: web3.PublicKey,
+    boardAccountPublicKey: web3.PublicKey,
+    firstPlayerAccountKp: web3.Keypair,
+    skipAirdrop = false
+  ) => {
+    if (!skipAirdrop) {
+      await airdrop(firstPlayerAccountKp.publicKey);
+    }
+    const firstPlayerJoinsTxHash = await program.methods
+      .requestHotPotato(minimumTicketEntry)
+      .accounts({
+        game: gameAccountPublicKey,
+        board: boardAccountPublicKey,
+        player: firstPlayerAccountKp.publicKey,
+        systemProgram: web3.SystemProgram.programId,
+      })
+      .signers([firstPlayerAccountKp])
+      .rpc()
+      .catch((e) => {
+        console.log(anchor.translateError(e, new Map()));
+        throw Error(e);
+      })
+      .then((txHash) => txHash);
+    const firstPlayerJoinsTxConfirmation = await confirmTx(
+      firstPlayerJoinsTxHash
+    );
+    const refetchedGameAccount =
+      await program.account.game.fetch(gameAccountPublicKey);
+    const refetchedboardAccount = await program.account.board.fetch(
+      refetchedGameAccount.board
+    );
+    return {
+      firstPlayerJoinsTxHash,
+      firstPlayerJoinsTxConfirmation,
+      refetchedGameAccount,
+      refetchedboardAccount,
+    };
+  };
+
   it("fails initializing if gameMaster is not signer", async () => {
     const gameMasterAccountKp = new web3.Keypair();
-    const someOtherAccountKp = new web3.Keypair();
+    await airdrop(gameMasterAccountKp.publicKey);
+    const boardAccountKp = await initializeBoardAccount(gameMasterAccountKp);
     const [gameAccountPublicKey] = web3.PublicKey.findProgramAddressSync(
       [gameMasterAccountKp.publicKey.toBuffer()],
       program.programId
     );
-
-    await airdrop(gameMasterAccountKp.publicKey);
+    const someOtherAccountKp = new web3.Keypair();
     await expect(
       program.methods
         .initialize(oneDay, oneHour, minimumTicketEntry)
         .accounts({
           newGame: gameAccountPublicKey,
+          newBoard: boardAccountKp.publicKey,
           gameMaster: gameMasterAccountKp.publicKey,
           systemProgram: web3.SystemProgram.programId,
         })
@@ -92,12 +189,16 @@ describe("HotPotato", () => {
 
   describe("Initializing", async () => {
     let gameMasterAccountKp: web3.Keypair;
+    let boardAccountPublicKey: web3.PublicKey;
     let gameAccount: Awaited<ReturnType<typeof program.account.game.fetch>>;
+    let boardAccount: Awaited<ReturnType<typeof program.account.board.fetch>>;
     let txHash: string;
     before(async () => {
       const res = await initLongGame();
       gameMasterAccountKp = res.gameMasterAccountKp;
+      boardAccountPublicKey = res.boardAccountPublicKey;
       gameAccount = res.gameAccount;
+      boardAccount = res.boardAccount;
       txHash = res.txHash;
     });
 
@@ -105,7 +206,16 @@ describe("HotPotato", () => {
       assert(gameMasterAccountKp.publicKey.equals(gameAccount.gameMaster)));
     it("has status is pending", () =>
       expect(gameAccount.state).to.eql({ pending: {} }));
-    it("has an empty board", () => expect(gameAccount.board).to.eql([]));
+    it("has a game board", () =>
+      expect(gameAccount.board).to.eql(boardAccountPublicKey));
+    it("has an empty hotPotatoHolders of length 10_000", () => {
+      expect(boardAccount.head).to.be.a.bignumber.that.is.eq(bigNumZero);
+      expect(boardAccount.tail).to.be.a.bignumber.that.is.eq(bigNumZero);
+      boardAccount.potatoHolders.every((holder) =>
+        expectEmptyBoardSlot(holder)
+      );
+      expect(boardAccount.potatoHolders.length).to.eq(10_000);
+    });
     it("has a staging period", () =>
       expect(gameAccount.stagingPeriodLength).to.be.a.bignumber.that.eq(
         oneDay
@@ -133,12 +243,16 @@ describe("HotPotato", () => {
     describe("Prohibited actions", async () => {
       let gameMasterAccountKp: web3.Keypair;
       let gameAccountPublicKey: web3.PublicKey;
+      let boardAccountPublicKey: web3.PublicKey;
       let someOtherAccountKp: web3.Keypair;
+      let boardAccountKp: web3.Keypair;
 
       before(async () => {
         const res = await initLongGame();
         gameAccountPublicKey = res.gameAccountPublicKey;
+        boardAccountPublicKey = res.boardAccountPublicKey;
         gameMasterAccountKp = res.gameMasterAccountKp;
+        boardAccountKp = res.boardAccountKp;
         someOtherAccountKp = new web3.Keypair();
       });
       it("fails cranking if gameMaster is not signer", async () =>
@@ -183,7 +297,9 @@ describe("HotPotato", () => {
             .requestHotPotato(minimumTicketEntry)
             .accounts({
               game: gameAccountPublicKey,
+              board: boardAccountPublicKey,
               player: gameMasterAccountKp.publicKey,
+              systemProgram: web3.SystemProgram.programId,
             })
             .signers([gameMasterAccountKp])
             .rpc()
@@ -194,20 +310,170 @@ describe("HotPotato", () => {
             .requestHotPotato(minimumTicketEntry.subn(1))
             .accounts({
               game: gameAccountPublicKey,
+              board: boardAccountPublicKey,
               player: someOtherAccountKp.publicKey,
+              systemProgram: web3.SystemProgram.programId,
             })
             .signers([someOtherAccountKp])
             .rpc()
-        ).to.be.rejectedWith(anchor.AnchorError, "InsufficientFunds"));
+        ).to.be.rejectedWith(anchor.AnchorError, "BelowTicketEntryMinimum"));
+      it("does not allow player to request hot potato with more than they have", async () => {
+        const playerAccount = new web3.Keypair();
+        await expect(
+          program.methods
+            .requestHotPotato(minimumTicketEntry)
+            .accounts({
+              game: gameAccountPublicKey,
+              board: boardAccountPublicKey,
+              player: playerAccount.publicKey,
+              systemProgram: web3.SystemProgram.programId,
+            })
+            .signers([playerAccount])
+            .rpc()
+        ).to.be.rejectedWith(Error, "custom program error: 0x1"); // InsufficientFunds
+      });
     });
-    it("changes status to staging when first player joins", async () => {});
-    it("logs status change to staging", async () => {});
-    it("logs player joining and amount", async () => {});
-    it("fills board with player's turns", async () => {});
+    describe("First player requests hot potato", async () => {
+      const amountWithoutChumpChange =
+        minimumTicketEntry.toNumber() -
+        (minimumTicketEntry.toNumber() % NumTurns);
+
+      it("changes status to staging with starting time when first player joins", async () => {
+        const { gameAccountPublicKey, boardAccountPublicKey } =
+          await initLongGame();
+        const firstPlayerAccountKp = new web3.Keypair();
+
+        const { firstPlayerJoinsTxConfirmation, refetchedGameAccount } =
+          await doFirstPlayerRequestHotPotato(
+            gameAccountPublicKey,
+            boardAccountPublicKey,
+            firstPlayerAccountKp
+          );
+
+        const txTime = await program.provider.connection.getBlockTime(
+          firstPlayerJoinsTxConfirmation.context.slot
+        );
+        expect(refetchedGameAccount.state).to.eql({
+          staging: { ending: new anchor.BN(txTime).add(oneDay) },
+        });
+      });
+      it("logs status change to staging", async () => {
+        const { gameAccountPublicKey, boardAccountPublicKey } =
+          await initLongGame();
+        const firstPlayerAccountKp = new web3.Keypair();
+
+        const { firstPlayerJoinsTxHash } = await doFirstPlayerRequestHotPotato(
+          gameAccountPublicKey,
+          boardAccountPublicKey,
+          firstPlayerAccountKp
+        );
+
+        const txDetails = await program.provider.connection.getTransaction(
+          firstPlayerJoinsTxHash,
+          {
+            maxSupportedTransactionVersion: 0,
+            commitment: "confirmed",
+          }
+        );
+        const logs = txDetails?.meta?.logMessages;
+        expect(logs.join("\n")).to.include("Game is now in staging mode");
+      });
+      it("transfer funds to the hotPotatoGame", async () => {
+        const { gameAccountPublicKey, boardAccountPublicKey } =
+          await initLongGame();
+        const firstPlayerAccountKp = new web3.Keypair();
+        await airdrop(firstPlayerAccountKp.publicKey);
+        const gameAccountStartingBalance =
+          await program.provider.connection.getBalance(gameAccountPublicKey);
+        const firstPlayerStartingBalance =
+          await program.provider.connection.getBalance(
+            firstPlayerAccountKp.publicKey
+          );
+
+        await doFirstPlayerRequestHotPotato(
+          gameAccountPublicKey,
+          boardAccountPublicKey,
+          firstPlayerAccountKp,
+          true
+        );
+        const gameAccountEndingBalance =
+          await program.provider.connection.getBalance(gameAccountPublicKey);
+        const firstPlayerEndingBalance =
+          await program.provider.connection.getBalance(
+            firstPlayerAccountKp.publicKey
+          );
+
+        expect(gameAccountEndingBalance).to.be.eq(
+          gameAccountStartingBalance + amountWithoutChumpChange
+        );
+        expect(firstPlayerEndingBalance).to.be.eq(
+          firstPlayerStartingBalance - amountWithoutChumpChange
+        );
+      });
+      it("logs player joining and amount", async () => {
+        const { gameAccountPublicKey, boardAccountPublicKey } =
+          await initLongGame();
+        const firstPlayerAccountKp = new web3.Keypair();
+
+        const { firstPlayerJoinsTxHash } = await doFirstPlayerRequestHotPotato(
+          gameAccountPublicKey,
+          boardAccountPublicKey,
+          firstPlayerAccountKp
+        );
+
+        const txDetails = await program.provider.connection.getTransaction(
+          firstPlayerJoinsTxHash,
+          {
+            maxSupportedTransactionVersion: 0,
+            commitment: "confirmed",
+          }
+        );
+        const logs = txDetails?.meta?.logMessages;
+
+        expect(logs.join("\n")).to.include(
+          `Player ${firstPlayerAccountKp.publicKey.toString()} joined with ${amountWithoutChumpChange}`
+        );
+      });
+      it("fills hotPotatoHolders with player's turn information once", async () => {
+        const { gameAccountPublicKey, boardAccountPublicKey } =
+          await initLongGame();
+        const firstPlayerAccountKp = new web3.Keypair();
+
+        const { refetchedboardAccount } = await doFirstPlayerRequestHotPotato(
+          gameAccountPublicKey,
+          boardAccountPublicKey,
+          firstPlayerAccountKp
+        );
+
+        const firstPlayerSlot = refetchedboardAccount.potatoHolders[0];
+        expect(firstPlayerSlot)
+          .to.have.a.property("player")
+          .and.to.eql(firstPlayerAccountKp.publicKey);
+        console.log("0");
+        expect(firstPlayerSlot)
+          .to.have.a.property("turnNumber")
+          .and.to.be.a.bignumber.that.is.eq(bigNumZero);
+        console.log("1");
+        expect(firstPlayerSlot)
+          .to.have.a.property("turnAmount")
+          .and.to.be.a.bignumber.that.is.eq(
+            new anchor.BN(amountWithoutChumpChange / NumTurns)
+          );
+        expect(refetchedboardAccount.head).to.be.a.bignumber.that.is.eq(
+          bigNumZero
+        );
+        expect(refetchedboardAccount.tail).to.be.a.bignumber.that.is.eq(
+          new anchor.BN(1)
+        );
+        refetchedboardAccount.potatoHolders
+          .slice(1)
+          .every((holder) => expectEmptyBoardSlot(holder));
+      });
+      it("does not allow first crank until after staging period", async () => {});
+    });
+    it("changes status to active on first crank", async () => {});
     it("allows first player to join again");
     it("allows second player to join");
-    it("does not allow first crank until after staging period", async () => {});
-    it("changes status to active on first crank", async () => {});
     // TODO(techiejd): Allow for affiliate links.
     it("sends SOL to first and second player after first crank", async () => {});
     it("allows third player to join", async () => {});
