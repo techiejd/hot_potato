cddc8c3cc484c2fe2efc659f48743aea3bb1f07e
techiejd <62455039+techiejd@users.noreply.github.com>
2024-06-03 21:41:03 +0700

    Adds payments page and linking

--- app/src/app/localHeader.tsx
+++ app/src/app/localHeader.tsx
@@ -1,18 +1,22 @@
 import { FC } from "react";
+import Link from "next/link";
 
 const LocalHeader: FC<{ page: "contribute" | "payments" }> = ({ page }) => (
-  <div className="self-stretch h-[42px] bg-cornsilk overflow-hidden shrink-0 flex flex-row justify-between px-[12px] md:px-[32px] box-border gap-[20px] text-left text-xs font-tahoma [text-decoration:none] font-bold">
+  <div className="self-stretch h-[42px] bg-cornsilk overflow-hidden shrink-0 flex flex-row justify-between px-[12px] md:px-[32px] box-border gap-[20px] text-left text-xs font-tahoma [text-decoration:none] font-bold mb-[48px]">
     <div className="flex flex-row items-start justify-start gap-[10px]">
-      <a
-        className={`self-stretch flex items-center justify-center ${page == "contribute" ? "border-b-2 border-solid" : "text-gray-2100 cursor-pointer"}`}
+      <Link
+        className={`[text-decoration:none] text-[inherit] self-stretch flex items-center justify-center ${page == "contribute" ? "border-b-2 border-solid" : "text-gray-2100 cursor-pointer"}`}
+        href="/"
       >
         Contribute
-      </a>
-      <a
-        className={`self-stretch flex items-center justify-center ${page == "payments" ? "border-b-2 border-solid" : "text-gray-2100 cursor-pointer"}`}
+      </Link>
+      <Link
+        className={`[text-decoration:none] text-[inherit] self-stretch flex items-center justify-center ${page == "payments" ? "border-b-2 border-solid" : "text-gray-2100 cursor-pointer"}`}
+        href="/payments"
+        {...(page == "payments" ? { "aria-current": "page" } : {})}
       >
         Payments
-      </a>
+      </Link>
     </div>
     <div className="flex flex-row items-start justify-start gap-[10px]">
       <a className="self-stretch flex items-center justify-center cursor-pointer">
---
+++ app/src/app/payments/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { Fragment } from "react";
+import LocalHeader from "../localHeader";
+import PonzuMessage from "../ponzuMessage";
+import { useWallet } from "@solana/wallet-adapter-react";
+import Image from "next/image";
+import { useWalletModal } from "@/wallet-adapter-react-ui";
+
+const Payments = () => {
+  const { connected, connecting } = useWallet();
+  const { setVisible } = useWalletModal();
+  return (
+    <Fragment>
+      <LocalHeader page="payments" />
+      <div className="self-stretch flex flex-row items-center justify-center grow">
+        <div className="self-stretch flex flex-col justify-between grow max-w-screen-md">
+          <div className="self-stretch flex flex-row items-start justify-start py-0 px-[33px] box-border max-w-full font-tahoma text-sm">
+            <div className="flex-1 bg-cornsilk box-border overflow-hidden flex flex-row items-start justify-between py-[21px] px-[26px] max-w-full gap-[20px] border-[1px] border-solid border-gray-1600">
+              <div className="text-gray-700">ponzu pay you</div>
+              {connected && (
+                <div className="w-[12.9px] h-[13.7px] animate-spin">
+                  <Image alt="" src="/circular-progress.svg" fill />
+                </div>
+              )}
+              {!connected && (
+                <a className="font-bold" onClick={() => setVisible(true)}>
+                  {connecting ? "connecting..." : "[connect wallet]"}
+                </a>
+              )}
+            </div>
+          </div>
+          <PonzuMessage />
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Payments;
