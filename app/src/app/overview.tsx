b051c5d6e51668501dc8272e5ed5c77afda2509d
techiejd <62455039+techiejd@users.noreply.github.com>
2024-06-05 16:29:33 +0700

    Adds the new overview which loads prior contributions

--- app/src/app/overview.tsx
+++ app/src/app/overview.tsx
@@ -1,68 +1,108 @@
+"use client";
+import { useProgramContext } from "@/program";
+import type { DocumentData } from "firebase/firestore";
 import Image from "next/image";
 import { FC } from "react";
+import RelativeTime from "@yaireo/relative-time";
 
-const DataRow = () => (
-  <tr>
-    <td>12s ago</td>
-    <td>2.0</td>
-    <td>HvFd...5jqDK</td>
-    <td className="flex">
-      <a className="w-full h-full cursor-pointer">
-        <div className="relative w-[12px] h-[12px] md:w-[18px] md:h-[18px]">
-          <Image alt="" src="/link-out.svg" fill />
+const DataRow: FC<{ contribution: DocumentData; now: RelativeTime }> = ({
+  contribution,
+  now,
+}) => {
+  const when = now.from(new Date(contribution.blockTime * 1000));
+  // We know the minimum is 0.5 SOL and amount is given in lamports.
+  const amount = ((a: string) => {
+    const padded = a.padStart(10, "0");
+    const rightOfDecimal = padded.slice(-9).slice(0, 2);
+    const leftOfDecimal = padded.slice(0, padded.length - 9);
+    return `${leftOfDecimal}.${rightOfDecimal}`;
+  })(contribution.ticketEntryAmount as string);
+  const playerAddy =
+    contribution.player.slice(0, 4) + "..." + contribution.player.slice(-4);
+  const isDevNet = useProgramContext().isDevNet;
+  console.log({
+    tx: contribution.transaction.signatures[0],
+    isDevNet,
+    combo:
+      contribution.transaction.signatures[0] +
+      (isDevNet ? "?cluster=devnet" : ""),
+    txLink: `https://explorer.solana.com/tx/${contribution.transaction.signatures[0] + isDevNet ? "?cluster=devnet" : ""}`,
+  });
+  const txLink = `https://explorer.solana.com/tx/${contribution.transaction.signatures[0] + (isDevNet ? "?cluster=devnet" : "")}`;
+  return (
+    <tr>
+      <td>{when}</td>
+      <td>{amount}</td>
+      <td>{playerAddy}</td>
+      <td className="flex">
+        <a
+          className="w-full h-full cursor-pointer"
+          href={txLink}
+          target="_blank"
+        >
+          <div className="relative w-[12px] h-[12px] md:w-[18px] md:h-[18px]">
+            <Image alt="" src="/link-out.svg" fill />
+          </div>
+        </a>
+      </td>
+    </tr>
+  );
+};
+
+const Overview: FC = () => {
+  const { contributions } = useProgramContext().cached;
+  const now = new RelativeTime();
+  console.log({ contributions });
+  return (
+    <div className="bg-cornsilk box-border overflow-hidden flex flex-col items-start justify-start p-[24px] gap-[12px] text-3xs md:text-sm border-[1px] border-solid border-gray-400">
+      <div className="text-sm md:text-base">overview</div>
+      <div className="w-full flex flex-col items-start justify-center gap-[2px] text-maroon">
+        <div className="w-full flex flex-row items-start justify-between">
+          <div>total contributions</div>
+          <b>1001.1 SOL</b>
         </div>
-      </a>
-    </td>
-  </tr>
-);
-
-const Overview: FC = () => (
-  <div className="bg-cornsilk box-border overflow-hidden flex flex-col items-start justify-start p-[24px] gap-[12px] text-3xs md:text-sm border-[1px] border-solid border-gray-400">
-    <div className="text-sm md:text-base">overview</div>
-    <div className="w-full flex flex-col items-start justify-center gap-[2px] text-maroon">
-      <div className="w-full flex flex-row items-start justify-between">
-        <div>total contributions</div>
-        <b>1001.1 SOL</b>
+        <div className="w-full flex flex-row items-start justify-between">
+          <div>ponzu balance</div>
+          <b>202 SOL</b>
+        </div>
+        <div className="w-full flex flex-row items-start justify-between">
+          <div>total payments</div>
+          <b>798 SOL</b>
+        </div>
       </div>
-      <div className="w-full flex flex-row items-start justify-between">
-        <div>ponzu balance</div>
-        <b>202 SOL</b>
+      <div className="flex flex-row items-center justify-start gap-[12px] text-4xs">
+        <h3 className="relative text-sm md:text-base font-normal font-inherit inline-block">
+          contributions
+        </h3>
+        <b>[see all]</b>
       </div>
-      <div className="w-full flex flex-row items-start justify-between">
-        <div>total payments</div>
-        <b>798 SOL</b>
-      </div>
+      <table className="w-full text-maroon text-left">
+        <thead>
+          <tr className="text-gray-700">
+            <th>when</th>
+            <th>SOL</th>
+            <th>player address</th>
+            <th className="pl-[3px]">tx</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contributions.length > 0 &&
+            contributions.map((contribution) => (
+              <DataRow
+                contribution={contribution}
+                now={now}
+                key={contribution.transaction.signatures[0]}
+              />
+            ))}
+          {contributions.length === 0 && (
+            <tr>
+              <td>Loading...</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
-    <div className="flex flex-row items-center justify-start gap-[12px] text-4xs">
-      <h3 className="relative text-sm md:text-base font-normal font-inherit inline-block">
-        contributions
-      </h3>
-      <b>[refresh]</b>
-      <b>[see all]</b>
-    </div>
-    <table className="w-full text-maroon text-left">
-      <thead>
-        <tr className="text-gray-700">
-          <th>date</th>
-          <th>SOL</th>
-          <th>address</th>
-          <th className="pl-[3px]">tx</th>
-        </tr>
-      </thead>
-      <tbody>
-        <DataRow />
-        <DataRow />
-        <DataRow />
-        <DataRow />
-        <DataRow />
-        <DataRow />
-        <DataRow />
-        <DataRow />
-        <DataRow />
-        <DataRow />
-      </tbody>
-    </table>
-  </div>
-);
+  );
+};
 
 export default Overview;
--- app/src/program/provider.tsx
+++ app/src/program/provider.tsx
@@ -11,21 +11,67 @@ import * as anchor from "@coral-xyz/anchor";
 import { HotPotato, IDL } from "./hot_potato";
 import { useConnection, useAnchorWallet } from "@solana/wallet-adapter-react";
 import { programId } from "./utils";
+import { initializeApp } from "firebase/app";
+import {
+  DocumentData,
+  collection,
+  connectFirestoreEmulator,
+  getFirestore,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnIC7cbxxVx32RySDktr4A4gtq8iEy4OA",
+  authDomain: "ponzu-b8783.firebaseapp.com",
+  projectId: "ponzu-b8783",
+  storageBucket: "ponzu-b8783.appspot.com",
+  messagingSenderId: "655475363783",
+  appId: "1:655475363783:web:eb6fc8d32f436c505a93e2",
+  measurementId: "G-HSR30KPR4M",
+};
+const app = initializeApp(firebaseConfig);
+const isDevEnvironment = process && process.env.NODE_ENV === "development";
+const firestore = (() => {
+  const firestore = getFirestore(app);
+  if (isDevEnvironment && !(firestore as any)._settingsFrozen)
+    // https://stackoverflow.com/questions/71574102/firebase-error-firestore-has-already-been-started-and-its-settings-can-no-lon
+    connectFirestoreEmulator(firestore, "localhost", 8080);
+  return firestore;
+})();
 
 interface ProgramContextState {
   provider: anchor.Provider | undefined;
   program: anchor.Program<HotPotato> | undefined;
+  isDevNet: boolean;
+  cached: {
+    contributions: DocumentData[];
+  };
 }
 
 const ProgramContext = createContext<ProgramContextState>({
   provider: undefined,
   program: undefined,
+  cached: { contributions: [] },
+  isDevNet: isDevEnvironment,
 });
 
 // Create a provider component
 export const ProgramProvider: FC<PropsWithChildren> = ({ children }) => {
   const { connection } = useConnection();
   const wallet = useAnchorWallet();
+  const q = query(
+    collection(firestore, "events"),
+    where("name", "==", "PotatoReceived"),
+    orderBy("blockTime", "desc"),
+    limit(10)
+  );
+  const [contributions, loading, error, snapshot] = useCollectionData(q, {
+    initialValue: [],
+  });
 
   const provider = useMemo(() => {
     if (!wallet) return;
@@ -44,6 +90,8 @@ export const ProgramProvider: FC<PropsWithChildren> = ({ children }) => {
       value={{
         provider,
         program,
+        cached: { contributions: contributions! },
+        isDevNet: isDevEnvironment,
       }}
     >
       {children}
